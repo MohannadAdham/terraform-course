3b0d25296c7e45caac3d43a1f9467edc7171f46a
Mohannad Adham <mohannad.adm@gmail.com>
2021-02-13 19:03:54 +0100

    Adopt default vpc and rename tf file to prod

--- prod.tf
+++ prod.tf
@@ -3,7 +3,11 @@ provider "aws" {
   region  = "eu-west-3"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-course-mohannad-2021"
   acl    = "private"
 }
+
+resource "aws_default_vpc" "defaul" {}
+
+
